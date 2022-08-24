8b52a11ed3250261adf851ce26fc624f89228939
Immanuel <immanueligwe@gmail.com>
2022-08-24 14:15:19 +0200

    Update dogChase.cs

--- Assets/dogChase.cs
+++ Assets/dogChase.cs
@@ -6,17 +6,18 @@ public class dogChase : MonoBehaviour
 {
     [SerializeField] private GameObject cat;
     [SerializeField] private float speed = 1.5f;
+    private Vector2 initialLocation;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5));
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, cat.transform.position, speed);
-        transform.right = cat.transform.position - transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, cat.transform.position, Time.deltaTime);
+        //transform.right = cat.transform.position - transform.position;
     }
 }
