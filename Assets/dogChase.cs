fd7798924cacc45e144a4be0c72c4aeb581dc2db
Immanuel <immanueligwe@gmail.com>
2022-08-24 14:57:49 +0200

    dog moving

--- Assets/dogChase.cs
+++ Assets/dogChase.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class dogChase : MonoBehaviour
 {
     [SerializeField] private GameObject cat;
-    [SerializeField] private float speed = 5f;
+    public Rigidbody2D rigidBody;
+    public float minDistance = 0.09f;
+    public float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
+        rigidBody = GetComponent<Rigidbody2D>();
         transform.position = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5));
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = Vector2.MoveTowards(transform.position, cat.transform.position, speed * Time.deltaTime);
+        Vector3 dir = (cat.transform.position - rigidBody.transform.position).normalized;
+        if (Vector3.Distance(cat.transform.position, rigidBody.transform.position) > minDistance)
+            rigidBody.MovePosition(rigidBody.transform.position + dir * speed * Time.fixedDeltaTime);
+        //transform.position = Vector2.MoveTowards(transform.position, cat.transform.position, speed * Time.deltaTime);
     }
 }
