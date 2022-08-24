e3ebbeb832c889c8dadbf9ae0fb5bf14786fad13
Immanuel <immanueligwe@gmail.com>
2022-08-24 14:40:32 +0200

    some changes

--- Assets/dogChase.cs
+++ Assets/dogChase.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class dogChase : MonoBehaviour
 {
     [SerializeField] private GameObject cat;
-    [SerializeField] private float speed = 1.5f;
-    private Vector2 initialLocation;
+    [SerializeField] private float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +16,6 @@ public class dogChase : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, cat.transform.position, Time.deltaTime);
-        //transform.right = cat.transform.position - transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, cat.transform.position, speed * Time.deltaTime);
     }
 }
