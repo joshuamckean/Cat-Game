928335f4f14b831c2509039f14300caaf05d62d2
joshuamckean <joshuamckean1@gmail.com>
2022-08-25 10:37:52 +0200

    hopefully fix text

--- Assets/StrengthTextScript.cs
+++ Assets/StrengthTextScript.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class StrengthTextScript : MonoBehaviour
 {
-    public Text MyText;
+    public TextMeshProUGUI MyText;
     public int strength;
     // Start is called before the first frame update
     void Start()
